e50b8efa797089ea83b953c779c7059d793549f8
James Donnelly <4444684+JDIZM@users.noreply.github.com>
2023-08-30 18:53:06 +0100

    feat: add sass to storybook

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,23 +1,30 @@
 module.exports = {
-  "stories": [
-    "../src/**/*.stories.mdx",
-    "../src/**/*.stories.@(js|jsx|ts|tsx)"
+  stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
+
+  addons: [
+    '@storybook/addon-links',
+    '@storybook/addon-essentials',
+    '@storybook/addon-interactions',
+    '@storybook/addon-mdx-gfm',
+    '@storybook/addon-essentials',
+    {
+      name: '@storybook/addon-styling',
+      options: {
+        sass: {
+          // Require your Sass preprocessor here
+          implementation: require('sass')
+        }
+      }
+    }
   ],
 
-  "addons": [
-    "@storybook/addon-links",
-    "@storybook/addon-essentials",
-    "@storybook/addon-interactions",
-    "@storybook/addon-mdx-gfm"
-  ],
-
-  "framework": {
-    name: "@storybook/vue3-vite",
+  framework: {
+    name: '@storybook/vue3-vite',
     options: {}
   },
 
-  "features": {
-    "storyStoreV7": true
+  features: {
+    storyStoreV7: true
   },
 
   docs: {
--- vitest.config.ts
+++ vitest.config.ts
@@ -12,7 +12,7 @@ export default mergeConfig(
       root: fileURLToPath(new URL('./', import.meta.url)),
       coverage: {
         provider: 'istanbul' // or 'v8'
-      },
+      }
     }
   })
 )
